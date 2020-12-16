2e1608cd0bcc9344eca3dae6bbfbe7ed647ba357
Trevor Fayas <tfayas@hbs.net>
2020-12-16 12:51:46 -0600

    items

---
+++ AutomaticGeneratedUserRoles.Mother/HBS.AutomaticGeneratedUserRoles.Mother/AutomaticGeneratedUserRoles.cs
@@ -0,0 +1,228 @@
+﻿using CMS;
+using CMS.DataEngine;
+using CMS.Membership;
+using CMS.Modules;
+using CMS.SiteProvider;
+using System.Linq;
+
+// Registers the custom module into the system
+[assembly: RegisterModule(typeof(AutomaticGeneratedUserRolesInitializationModule))]
+
+public class AutomaticGeneratedUserRolesInitializationModule : Module
+{
+    const string _AuthenticatedRole = "_authenticated_";
+    const string _NotAuthenticatedRole = "_notauthenticated_";
+    const string _EveryoneRole = "_everyone_";
+
+    const string _AuthenticatedRoleDisplayName = "Authenticated users";
+    const string _NotAuthenticatedRoleDisplayName = "Not authenticated users";
+    const string _EveryoneRoleDisplayName = "Everyone";
+
+    const string _AuthenticatedRoleDescription = "Special role which automatically covers all authenticated users.";
+    const string _NotAuthenticatedRoleDescription = "Special role which automatically covers all not authenticated users.";
+    const string _EveryoneRoleDescription = "Special role which automatically covers all users.";
+
+    // Module class constructor, the system registers the module under the name "CustomInit"
+    public AutomaticGeneratedUserRolesInitializationModule()
+        : base("AutomaticGeneratedUserRolesInitializationModule_Mother")
+    {
+    }
+
+    // Contains initialization code that is executed when the application starts
+    protected override void OnInit()
+    {
+        base.OnInit();
+
+        // Assigns custom handlers to events
+        EnsureAuthenticatedRole();
+
+        // Handle Global Role assignment
+        UserInfo.TYPEINFO.Events.Insert.After += User_Insert_After;
+
+        // Handle User Site specific role assignment
+        UserSiteInfo.TYPEINFO.Events.Insert.After += UserSite_Insert_After;
+
+        // Handle when new site is created to create roles.
+        SiteInfo.TYPEINFO.Events.Insert.After += Site_Insert_After;
+
+        // Nuget Manifest Build
+        ModulePackagingEvents.Instance.BuildNuSpecManifest.After += BuildNuSpecManifest_After;
+    }
+
+    private void BuildNuSpecManifest_After(object sender, BuildNuSpecManifestEventArgs e)
+    {
+        if (e.ResourceName.Equals("HBS.AutomaticGeneratedUserRoles.Mother", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            e.Manifest.Metadata.ProjectUrl = "https://github.com/KenticoDevTrev/AutomaticGenericUserRoles";
+            e.Manifest.Metadata.IconUrl = "https://raw.githubusercontent.com/Kentico/devnet.kentico.com/master/marketplace/assets/generic-integration.png";
+            e.Manifest.Metadata.ReleaseNotes = "Created Mother specific version so would not corrupt admin/mother instance upon install.";
+        }
+    }
+
+    private void UserSite_Insert_After(object sender, ObjectEventArgs e)
+    {
+        UserSiteInfo UserSite = (UserSiteInfo)e.Object;
+        UserInfo User = UserInfoProvider.GetUserInfo(UserSite.UserID);
+
+        if (User.UserName.Equals("public", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            // Add to unauthenticated
+            RoleInfo NotAuthenticatedUserRole = GetOrCreateRole(_NotAuthenticatedRole, _NotAuthenticatedRoleDisplayName, _NotAuthenticatedRoleDescription, UserSite.SiteID);
+            HandleUserRole(UserSite.UserID, NotAuthenticatedUserRole.RoleID);
+        }
+        else
+        {
+            // Add to authenticated
+            RoleInfo AuthenticatedUserRole = GetOrCreateRole(_AuthenticatedRole, _AuthenticatedRoleDisplayName, _AuthenticatedRoleDescription, UserSite.SiteID);
+            HandleUserRole(UserSite.UserID, AuthenticatedUserRole.RoleID);
+        }
+
+        // Add to everyone
+        RoleInfo EveryoneUserRole = GetOrCreateRole(_EveryoneRole, _EveryoneRoleDisplayName, _EveryoneRoleDescription, UserSite.SiteID);
+        HandleUserRole(UserSite.UserID, EveryoneUserRole.RoleID);
+    }
+
+    private void Site_Insert_After(object sender, ObjectEventArgs e)
+    {
+        // Create new roles
+        SiteInfo Site = (SiteInfo)e.Object;
+        RoleInfo AuthenticatedUserRole = GetOrCreateRole(_AuthenticatedRole, _AuthenticatedRoleDisplayName, _AuthenticatedRoleDescription, Site.SiteID);
+        RoleInfo NotAuthenticatedUserRole = GetOrCreateRole(_NotAuthenticatedRole, _NotAuthenticatedRoleDisplayName, _NotAuthenticatedRoleDescription, Site.SiteID);
+        RoleInfo EveryoneUserRole = GetOrCreateRole(_EveryoneRole, _EveryoneRoleDisplayName, _EveryoneRoleDescription, Site.SiteID);
+    }
+
+    private void User_Insert_After(object sender, ObjectEventArgs e)
+    {
+        RoleInfo AuthenticatedUserRole = GetOrCreateRole(_AuthenticatedRole, _AuthenticatedRoleDisplayName, _AuthenticatedRoleDescription, null);
+        RoleInfo EveryoneRole = GetOrCreateRole(_EveryoneRole, _EveryoneRoleDisplayName, _EveryoneRoleDescription, null);
+        HandleUserRole(((UserInfo)e.Object).UserID, AuthenticatedUserRole.RoleID);
+        HandleUserRole(((UserInfo)e.Object).UserID, EveryoneRole.RoleID);
+    }
+
+    private void EnsureAuthenticatedRole()
+    {
+        // Handle Global First
+        RoleInfo AuthenticatedUserRole = GetOrCreateRole(_AuthenticatedRole, _AuthenticatedRoleDisplayName, _AuthenticatedRoleDescription, null);
+        RoleInfo NotAuthenticatedUserRole = GetOrCreateRole(_NotAuthenticatedRole, _NotAuthenticatedRoleDisplayName, _NotAuthenticatedRoleDescription, null);
+        RoleInfo EveryoneUserRole = GetOrCreateRole(_EveryoneRole, _EveryoneRoleDisplayName, _EveryoneRoleDescription, null);
+
+        // Public user
+        UserInfoProvider.GetUsers()
+            .WhereEquals("username", "public")
+            .WhereNotIn("UserID", UserRoleInfoProvider.GetUserRoles().WhereEquals("RoleID", NotAuthenticatedUserRole.RoleID)
+            .TypedResult.Select(x => x.UserID).ToArray()).ForEachObject(x =>
+            {
+                HandleUserRole(x.UserID, NotAuthenticatedUserRole.RoleID);
+            });
+
+        // Non public users
+        UserInfoProvider.GetUsers()
+            .WhereNotEquals("username", "public")
+            .WhereNotIn("UserID", UserRoleInfoProvider.GetUserRoles().WhereEquals("RoleID", AuthenticatedUserRole.RoleID).TypedResult.Select(x => x.UserID).ToArray()).ForEachObject(x =>
+            {
+                HandleUserRole(x.UserID, AuthenticatedUserRole.RoleID);
+            });
+
+        // Now everyone
+        UserInfoProvider.GetUsers()
+            .WhereNotIn("UserID", UserRoleInfoProvider.GetUserRoles().WhereEquals("RoleID", EveryoneUserRole.RoleID).TypedResult.Select(x => x.UserID).ToArray()).ForEachObject(x =>
+            {
+                HandleUserRole(x.UserID, EveryoneUserRole.RoleID);
+            });
+
+        // Now go through Site Users
+        foreach (SiteInfo Site in SiteInfoProvider.GetSites())
+        {
+            RoleInfo SiteAuthenticatedUserRole = GetOrCreateRole(_AuthenticatedRole, _AuthenticatedRoleDisplayName, _AuthenticatedRoleDescription, Site.SiteID);
+            RoleInfo SiteNotAuthenticatedUserRole = GetOrCreateRole(_NotAuthenticatedRole, _NotAuthenticatedRoleDisplayName, _NotAuthenticatedRoleDescription, Site.SiteID);
+            RoleInfo SiteEveryoneUserRole = GetOrCreateRole(_EveryoneRole, _EveryoneRoleDisplayName, _EveryoneRoleDescription, Site.SiteID);
+
+            // Public user
+            UserInfoProvider.GetUsers()
+                .WhereEquals("username", "public")
+                .WhereIn("UserID", UserSiteInfoProvider.GetUserSites().WhereEquals("SiteID", Site.SiteID).TypedResult.Select(x => x.UserID).ToArray())
+                .WhereNotIn("UserID", UserRoleInfoProvider.GetUserRoles().WhereEquals("RoleID", SiteAuthenticatedUserRole.RoleID)
+                .TypedResult.Select(x => x.UserID).ToArray()).ForEachObject(x =>
+                {
+                    HandleUserRole(x.UserID, SiteAuthenticatedUserRole.RoleID);
+                });
+
+            // Non public users
+            UserInfoProvider.GetUsers()
+                .WhereNotEquals("username", "public")
+                .WhereIn("UserID", UserSiteInfoProvider.GetUserSites().WhereEquals("SiteID", Site.SiteID).TypedResult.Select(x => x.UserID).ToArray())
+                .WhereNotIn("UserID", UserRoleInfoProvider.GetUserRoles().WhereEquals("RoleID", SiteNotAuthenticatedUserRole.RoleID).TypedResult.Select(x => x.UserID).ToArray()).ForEachObject(x =>
+                {
+                    HandleUserRole(x.UserID, SiteNotAuthenticatedUserRole.RoleID);
+                });
+
+            // Now everyone
+            UserInfoProvider.GetUsers()
+                .WhereIn("UserID", UserSiteInfoProvider.GetUserSites().WhereEquals("SiteID", Site.SiteID).TypedResult.Select(x => x.UserID).ToArray())
+                .WhereNotIn("UserID", UserRoleInfoProvider.GetUserRoles().WhereEquals("RoleID", SiteEveryoneUserRole.RoleID).TypedResult.Select(x => x.UserID).ToArray()).ForEachObject(x =>
+                {
+                    HandleUserRole(x.UserID, SiteEveryoneUserRole.RoleID);
+                });
+        }
+
+    }
+
+    /// <summary>
+    /// Helper to Get or create the given Role
+    /// </summary>
+    /// <param name="RoleCodeName">Role Code Name</param>
+    /// <param name="RoleDisplayName">Role Display Name</param>
+    /// <param name="RoleDescription">Role Description</param>
+    /// <returns>The RoleInfo</returns>
+    private RoleInfo GetOrCreateRole(string RoleCodeName, string RoleDisplayName, string RoleDescription, int? SiteID)
+    {
+        if (SiteID.HasValue)
+        {
+            // Get Site Specific Role
+            RoleInfo Role = RoleInfoProvider.GetRoleInfo(RoleCodeName, SiteID.Value);
+            if (Role == null)
+            {
+                Role = new RoleInfo()
+                {
+                    RoleDisplayName = RoleDisplayName,
+                    RoleName = RoleCodeName,
+                    RoleDescription = RoleDescription,
+                    SiteID = SiteID.Value,
+                    RoleIsDomain = false
+                };
+
+                RoleInfoProvider.SetRoleInfo(Role);
+            }
+            return Role;
+        }
+        else
+        {
+            // Get Global Role
+            RoleInfo Role = RoleInfoProvider.GetRoles().WhereEquals("RoleName", RoleCodeName).WhereNull("SiteID").FirstOrDefault();
+            if (Role == null)
+            {
+                Role = new RoleInfo()
+                {
+                    RoleDisplayName = RoleDisplayName,
+                    RoleName = RoleCodeName,
+                    RoleDescription = RoleDescription,
+                    RoleIsDomain = false
+                };
+
+                RoleInfoProvider.SetRoleInfo(Role);
+            }
+            return Role;
+        }
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="UserID"></param>
+    /// <param name="RoleID"></param>
+    private void HandleUserRole(int UserID, int RoleID)
+    {
+        UserRoleInfoProvider.AddUserToRole(UserID, RoleID);
+    }
+}
---
+++ AutomaticGeneratedUserRoles.Mother/HBS.AutomaticGeneratedUserRoles.Mother/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using CMS;
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HBS.AutomaticGeneratedUserRoles.Kentico.Mother")]
+[assembly: AssemblyDescription("Restores Kentico 12s generic Roles of _authenticated_ _notauthenticated_ and _everyone_ with code to automatically create and maintain these listings.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Heartland Business Systems")]
+[assembly: AssemblyProduct("Automatic Generated User Roles")]
+[assembly: AssemblyCopyright("2019 Heartland Business Systems")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDiscoverable]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96040a1d-8ccc-48ea-a5f3-14eeee1b4262")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("12.29.1.0")]
+[assembly: AssemblyFileVersion("12.29.1.0")]
